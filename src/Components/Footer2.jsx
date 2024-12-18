ed71f1178602880db2b0b83bc09bf284d35a9bc5
Hetal Patel <YOUR EMAIL ADDRESS>
2024-12-18 11:42:44 +0000

    try to solveld error

--- src/Components/Footer2.jsx
+++ src/Components/Footer2.jsx
@@ -1,11 +1,9 @@
-
-export default function Footer2(){
-    return(
+export default function Footer2() {
+    return (
         <>
-          <footer className="footer bg-neutral-950 text-neutral-content items-center p-4">
+            <footer className="footer bg-neutral-950 text-neutral-content items-center p-4">
                 <aside className="grid-flow-col items-center p-10 ml-10">
-
-                    <p> {new Date().getFullYear()} -Hetal&apos s Home Kitchen</p>
+                    <p>{new Date().getFullYear()} - Hetal&apos;s Home Kitchen</p>
                 </aside>
             </footer>
         </>
